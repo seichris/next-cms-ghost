ec68b9a48d59ff908ef7ff30ca9a94749aa9c230
Chris Seifert <25006584+seichris@users.noreply.github.com>
2021-03-07 12:06:25 +0100

    update Navbar socialLinks with Discord instead of Facebook

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,5 +1,5 @@
 import { TwitterIcon } from '@icons/TwitterIcon'
-import { FacebookIcon } from '@icons/FacebookIcon'
+import { DiscordIcon } from '@icons/DiscordIcon'
 
 import { SocialRss } from '@components/SocialRss'
 import { GhostSettings } from '@lib/ghost'
@@ -11,18 +11,16 @@ interface SocialLinkProps {
 
 export const SocialLinks = ({ siteUrl, site }: SocialLinkProps) => {
   const twitterUrl = site.twitter && `https://twitter.com/${site.twitter.replace(/^@/, ``)}`
-  const facebookUrl = site.facebook && `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}`
+  const discordUrl = "https://discord.gg/asgHzx2xDa"
 
   const { processEnv } = site
   const { memberSubscriptions } = processEnv
 
   return (
     <>
-      {site.facebook && (
-        <a href={facebookUrl} className="social-link social-link-fb" target="_blank" rel="noopener noreferrer" title="Facebook">
-          <FacebookIcon />
-        </a>
-      )}
+      <a href={discordUrl} className="social-link social-link-fb" target="_blank" rel="noopener noreferrer" title="Discord">
+        <DiscordIcon />
+      </a>
       {site.twitter && (
         <a href={twitterUrl} className="social-link social-link-tw" target="_blank" rel="noopener noreferrer" title="Twitter">
           <TwitterIcon />
